8403bec7090ca74da391c6de7ec9cf1087497ede
Martin521 <29605222+Martin521@users.noreply.github.com>
2024-03-12 14:27:14 +0100

    Some cleanup of the build script, and added vs code build task

--- build.fsx
+++ build.fsx
@@ -1,6 +1,6 @@
-// For now just creating the full doc with freshly numbered section headers
+// For now just creating the full doc with freshly numbered section headers and ToC
 // For now just a single script
-// TODO: boilerplate, index with links, reference links ...
+// TODO: boilerplate, reference links, annexes ...
 
 open System
 open System.Text.RegularExpressions
@@ -16,38 +16,46 @@ let initialState = {section = [0]; toc = tocHead; errors = []}
 
 let specDir = "spec"
 let outDir = "artifacts"
-let specFilePath filename = $"{specDir}/{filename}"
-let clauseFilePath clauseName = specFilePath clauseName + ".md"
-let clauseCatalogPath = specFilePath "clauses.json"
-let outFile = $"{outDir}/spec.md"
+let specPath filename = $"{specDir}/{filename}"
+let clausePath clauseName = specPath clauseName + ".md"
+let clauseCatalogPath = specPath "clauses.json"
+let outFilePath = $"{outDir}/spec.md"
 
 let readClauses() =
     try
         use clauseStream = File.OpenRead clauseCatalogPath
         let clauseCatalog = JsonSerializer.Deserialize<ClauseCatalog> clauseStream
-        let getClause name = {name = name; lines = File.ReadAllLines(clauseFilePath name) |> Array.toList}
+        let getClause name = {name = name; lines = File.ReadAllLines(clausePath name) |> Array.toList}
         let clauses = clauseCatalog.MainBody |> List.map getClause  // For now. TODO: Annexes etc.
         printfn $"read {clauses.Length} files with a total of {clauses |> List.sumBy (_.lines >> List.length)} lines"
-        clauses
+        Ok clauses
     with
-        e -> failwith e.Message
+        e -> Error e.Message
+
+let writeSpec (lines: string list) =
+    try
+        if not <| Directory.Exists outDir then Directory.CreateDirectory outDir |> ignore
+        File.WriteAllLines(outFilePath, lines)
+        Ok ()
+    with
+        e -> Error e.Message
 
 let rec newSection level prevSection =
-    match prevSection with
-    | [] -> [1]
-    | h::t when prevSection.Length = level -> (h + 1)::t
-    | h::t when prevSection.Length > level -> newSection level t
-    | h::t when prevSection.Length = level - 1 -> 1::prevSection
-    | _ -> []
+    match List.rev prevSection with
+        | [] -> [1]
+        | h::t when prevSection.Length = level -> (h + 1)::t
+        | _::t when prevSection.Length > level -> newSection level t
+        | _ when prevSection.Length = level - 1 -> 1::prevSection
+        | _ -> []
+    |> List.rev
 
-let referencable (s: string) =
-    seq {
+let referenceable (s: string) =
+    String [|
         for c in s do
             if Char.IsAsciiLetterLower c || c = '-' || Char.IsAsciiDigit c then yield c
             if Char.IsAsciiLetterUpper c then yield Char.ToLower c
             if c = ' ' then yield '-'
-        }
-    |> Seq.toArray |> String
+    |]
 
 let renumberIfHeaderLine clauseName state line =
     let m = Regex.Match(line, "^(#+) (\d.*)")
@@ -57,19 +65,17 @@ let renumberIfHeaderLine clauseName state line =
         let rest = m.Groups[2].Value
         let m = Regex.Match(rest, "([\.\d]*) ?(.*)")
         if m.Success then
+            let headerText = m.Groups[2].Value
             let section = newSection level state.section
             if section.IsEmpty then
                 let error = $"The header level jumps in {clauseName} from {state.section.Length} to {level}: {line}"
                 line, {state with errors = error::state.errors}
             else
-            let rSection = List.rev section
-            if rSection.IsEmpty then printfn $"empty section: {clauseName} {state} {line}"
-            let sText = $"""{rSection.Head}.{rSection.Tail |> List.map string |> String.concat "."}"""
-            let headerText = m.Groups[2].Value
-            let headerLine = $"{headerPrefix} {sText} {headerText}"
-            let anchor = $"#{referencable sText}-{referencable headerText}"
-            let tocLine = String.replicate (level - 1) "  " + $"- [{sText} {headerText}]({anchor})"
-            headerLine, {state with section = section; toc = tocLine::state.toc}
+                let sectionText = $"""{section.Head}.{section.Tail |> List.map string |> String.concat "."}"""
+                let headerLine = $"{headerPrefix} {sectionText} {headerText}"
+                let anchor = $"#{referenceable sectionText}-{referenceable headerText}"
+                let tocLine = String.replicate (level - 1) "  " + $"- [{sectionText} {headerText}]({anchor})"
+                headerLine, {state with section = section; toc = tocLine::state.toc}
         else failwith $"unexpected regex failure: {line}"
     else line, state
 
@@ -77,16 +83,31 @@ let renumberClause state clause =
     let outLines, state = (state, clause.lines) ||> List.mapFold (renumberIfHeaderLine clause.name)
     {clause with lines = outLines}, state
 
+let processClauses clauses =
+    (initialState, clauses) ||> List.mapFold renumberClause
+
+let createSpec processedClauses state =
+    List.rev state.toc @ List.collect (fun clause -> ""::clause.lines) processedClauses
+
 let build() =
-    let clauses = readClauses()
-    let (outClauses, state) = (initialState, clauses) ||> List.mapFold renumberClause
-    if state.errors.Length > 0 then
-        state.errors |> List.rev |> List.iter (printfn "%s")
-    else
-    let total = List.rev state.toc @ List.collect (fun clause -> ""::clause.lines) outClauses
-    if not <| Directory.Exists outDir then Directory.CreateDirectory outDir |> ignore
-    File.WriteAllLines(outFile, total)
-    printfn $"created {outFile}"
+    match readClauses() with
+    | Error msg ->
+        printfn $"ERROR: {msg}"
+        1
+    | Ok clauses ->
+        let (processedClauses, state) = processClauses clauses
+        if state.errors.Length > 0 then
+            state.errors |> List.rev |> List.iter (printfn "%s")
+            2
+        else
+            let specLines = createSpec processedClauses state
+            match writeSpec specLines with
+            | Error msg ->
+                printfn $"ERROR: {msg}"
+                1
+            | Ok _ ->
+                printfn $"created {outFilePath}"
+                0
 
 build()
 
